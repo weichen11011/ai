2333112b6e9f9c6fdd376a0ee6ea42b77accf11e
weichen11011 <99935264+weichen11011@users.noreply.github.com>
2024-06-18 17:16:16 +0800

    Update groqChat.py

--- hw9/groqChat.py
+++ hw9/groqChat.py
@@ -2,24 +2,29 @@ import os
 import sys
 from groq import Groq
 
-while True:
-    language = sys.argv[1].lower()
-    question = " ".join(sys.argv[2:])
-    print(f"選擇的語言：{language}")
-    print("問題：", question)
 
-    client = Groq(
-        api_key="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",
-    )
 
-    chat_completion = client.chat.completions.create(
-        messages=[
-            {
-                "role": "user",
-                "content": question,
-            }
-        ],
-        model="llama3-8b-8192",
-    )
+language = sys.argv[1].lower()
+if (language == 'ch'):
+    question = " ".join(sys.argv[2:]) + "請用中文回答"
+elif (language == 'en'):
+    question = " ".join(sys.argv[2:]) + "請用英文回答"
+print(f"選擇的語言：{language}")
+print("問題：", question)
 
-    print(chat_completion.choices[0].message.content)
+client = Groq(
+    api_key="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",
+)
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": question,
+            
+        }
+    ],
+    model="llama3-8b-8192",
+)
+
+print(chat_completion.choices[0].message.content)
