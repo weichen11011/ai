ca888830a2680270c7882bac938fae886966880c
weichen11011 <99935264+weichen11011@users.noreply.github.com>
2024-06-20 14:29:27 +0800

    Update cartpole.py

--- hw8/cartpole.py
+++ hw8/cartpole.py
@@ -4,7 +4,7 @@ Created on Sun May  5 06:13:01 2024
 
 @author: yan10
 """
-
+# 修改至 https://github.com/ccc112b/py2cs/blob/master/03-%E4%BA%BA%E5%B7%A5%E6%99%BA%E6%85%A7/06-%E5%BC%B7%E5%8C%96%E5%AD%B8%E7%BF%92/01-%E5%BC%B7%E5%8C%96%E5%AD%B8%E7%BF%92/01-gym/04-run/cartpole_human_run.py
 import gymnasium as gym
 env = gym.make("CartPole-v1", render_mode="human") # 若改用這個，會畫圖
 # env = gym.make("CartPole-v1", render_mode="rgb_array")
