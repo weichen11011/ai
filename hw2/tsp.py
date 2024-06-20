1916e226c87a48d75ecfc6bf68cc3b883df30abc
weichen11011 <99935264+weichen11011@users.noreply.github.com>
2024-06-20 14:28:18 +0800

    Update tsp.py

--- hw2/tsp.py
+++ hw2/tsp.py
@@ -4,6 +4,7 @@ Created on Mon Mar 18 14:48:16 2024
 
 @author: yan10
 """
+# 參考 https://github.com/ccc112b/py2cs/blob/master/03-%E4%BA%BA%E5%B7%A5%E6%99%BA%E6%85%A7/02-%E5%84%AA%E5%8C%96%E7%AE%97%E6%B3%95/01-%E5%82%B3%E7%B5%B1%E5%84%AA%E5%8C%96%E6%96%B9%E6%B3%95/01-%E5%84%AA%E5%8C%96/01-%E7%88%AC%E5%B1%B1%E6%BC%94%E7%AE%97%E6%B3%95/03-%E9%80%9A%E7%94%A8%E7%9A%84%E7%88%AC%E5%B1%B1%E6%A1%86%E6%9E%B6/tsp.py
 import random
 citys = [
     (0,3),(0,0),
